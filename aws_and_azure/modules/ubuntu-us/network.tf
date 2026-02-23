6271222fc74494805bf894f6ae7072c2e6b77d95
gkullio <105679126+gkullio@users.noreply.github.com>
2026-02-23 07:21:10 -0800

    updates to network and boilerplate

--- aws_and_azure/modules/ubuntu-us/network.tf
+++ aws_and_azure/modules/ubuntu-us/network.tf
@@ -31,16 +31,7 @@ resource "azurerm_network_security_group" "management_nsg" {
     owner = var.resourceOwner
   }
 }
-/*
-resource "azurerm_network_security_group" "internal_nsg" {
-  name                = "ubuntu-internal-NSG"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags = {
-    owner = var.resourceOwner
-  }
-}
-*/
+
 # Create network interface
 resource "azurerm_network_interface" "management_nic" {
   name                = "ubuntu-management-nic"
@@ -57,62 +48,10 @@ resource "azurerm_network_interface" "management_nic" {
     owner = var.resourceOwner
   }
 }
-/*
-resource "azurerm_network_interface" "internal_nic" {
-  name                = "ubuntu-internal-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
 
-  ip_configuration {
-    name                          = "internal_nic_configuration"
-    subnet_id                     = var.int_subnet_id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.245.2.50"
-    primary                       = true
-  }
-  ip_configuration {
-    name                          = "internal_nic_configuration_1"
-    subnet_id                     = var.int_subnet_id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.245.2.100"
-  }
-  ip_configuration {
-    name                          = "internal_nic_configuration_2"
-    subnet_id                     = var.int_subnet_id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.245.2.101"
-  }
-  ip_configuration {
-    name                          = "internal_nic_configuration_3"
-    subnet_id                     = var.int_subnet_id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.245.2.102"
-  }
-  ip_configuration {
-    name                          = "internal_nic_configuration_4"
-    subnet_id                     = var.int_subnet_id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.245.2.103"
-  }
-  ip_configuration {
-    name                          = "internal_nic_configuration_5"
-    subnet_id                     = var.int_subnet_id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.245.2.104"
-  }
-  tags = {
-    owner = var.resourceOwner
-  }
-}
-*/
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "mgmt" {
   network_interface_id      = azurerm_network_interface.management_nic.id
   network_security_group_id = azurerm_network_security_group.management_nsg.id
 }
-/*
-resource "azurerm_network_interface_security_group_association" "internal" {
-  network_interface_id      = azurerm_network_interface.internal_nic.id
-  network_security_group_id = azurerm_network_security_group.internal_nsg.id
-}
-*/
+
